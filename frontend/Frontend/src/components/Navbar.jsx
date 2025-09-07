093564964fb14b49c593c8655f8191f80e354680
Tomas Lopez <lopeztomas.dev@gmail.com>
2025-09-07 19:43:03 -0300

    Se creo la estructura para el frontend

---
+++ frontend/Frontend/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+
+export const Navbar = () => {
+  return (
+    <nav className="bg-sky-900 font-semibold  pr-8 text-white items-center flex gap-3 justify-end">
+      <a className="mt-2 mb-2 p-0 hover:bg-sky-950 hover:transition-colors " href="">inicio</a>
+      <a href="">about me</a>
+      <a href="">contact</a>
+    </nav>
+  )
+}
