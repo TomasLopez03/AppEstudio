4c1e82a1be785d699dee94cfd02afb509a3d6a05
Tomas Lopez <lopeztomas.dev@gmail.com>
2025-09-13 21:28:27 -0300

    Correción de los campos del modelo

--- backend/backend/apps/users/models.py
+++ backend/backend/apps/users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # User model.
-class User(AbstractUser ,models.Model):
+class User(AbstractUser):
     ROLE_CHOICES = [
         ('admin', 'Admin'),
         ('client', 'Client'),
@@ -10,14 +10,8 @@ class User(AbstractUser ,models.Model):
     ]
 
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='client')
-    id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=150, unique=True)
     email = models.EmailField(unique=True, null=False)
-    password = models.CharField(max_length=128)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
     razon_social = models.CharField(max_length=100, null=True)
     celular = models.IntegerField(null=True)
     cuit = models.CharField(max_length=20, null=False, unique=True)
-    is_active = models.BooleanField(default=True)
 
