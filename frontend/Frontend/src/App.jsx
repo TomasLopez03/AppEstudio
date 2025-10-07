23238ca30c8db690a44e397afe0c9c86ca480c14
Tomas Lopez <lopeztomas.dev@gmail.com>
2025-10-07 11:56:01 -0300

    Se actualizo Login.
    Se mejoro la experiencia de usuario al iniciar sesion,
    ahora se muestra un mensaje de error si no se ingresan los datos
    y el boton de iniciar sesion muestra un spinner
    mientras se procesa la solicitud.

--- frontend/Frontend/src/App.jsx
+++ frontend/Frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { AuthProvider } from "./auth/AuthContext.jsx"
 import { AdminDashboard } from "./pages/Dashboard/AdminDashboard.jsx"
 import { EmployeeDashboard } from "./pages/Dashboard/EmployeeDashboard.jsx"
 import { ClientDashboard } from "./pages/Dashboard/ClientDashboard.jsx"
+import { Home } from "./components/Home.jsx"
 
 function App() {
 
@@ -12,9 +13,10 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login />} />
-          <Route path="/admin" element={<AdminDashboard/>} />
-          <Route path="/employee" element={<EmployeeDashboard/>} />
-          <Route path="/client" element={<ClientDashboard/>} />
+          <Route path="/admin" element={<AdminDashboard />} />
+          <Route path="/employee" element={<EmployeeDashboard />} />
+          <Route path="/client" element={<ClientDashboard />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
